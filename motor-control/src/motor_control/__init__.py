8e064421f6cc80b4fb13a4153e978ed32a8d058d
Trevor Vannoy <trevorvannoy@montana.edu>
2024-04-30 17:00:57 -0600

    import module when importing package
    This lets us do `import motor_control` and have access to the
    module-level functions like `motor_control.connect()`. Otherwise, we
    would have to import as `from motor_control import motor_control`, which
    is annoying for such a small package with only one module. Basically,
    the module acts like a class...

---
+++ motor-control/src/motor_control/__init__.py
@@ -0,0 +1,5 @@
+# Import the module when importing the package so we don't have to worry about
+# importing the package and the module, i.e.,
+# `from motor_control import motor_control`
+from .motor_control import *
+
